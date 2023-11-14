9aa6b5e44a52a8785a2de1980824f5bfe24d83fb
ClC <144094751+b11009061@users.noreply.github.com>
2023-11-14 15:01:36 +0800

    Update todo.py

--- src/todo.py
+++ src/todo.py
@@ -1,12 +1,23 @@
-class Todo:
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
-        self.completed = False
+import requests
 
-    def mark_as_completed(self):
-        self.completed = True
+class NBAApi:
+    def __init__(self):
+        self.base_url = "https://stats.nba.com/stats"
 
-    def __str__(self):
-        status = "Completed" if self.completed else "Not Completed"
-        return f"Title: {self.title}, Description: {self.description}, Status: {status}"
+    def get_live_scores(self):
+        endpoint = "/scoreboardV2"
+        params = {
+            "gameDate": "today",
+            "leagueId": "00",
+            "dayOffset": "0",
+        }
+
+        url = f"{self.base_url}{endpoint}"
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print(f"Error: {response.status_code}")
+            return None

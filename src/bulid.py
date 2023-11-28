71d3612a825be6888808733c1b8575d52a736d44
H1s0kaaa <144766512+H1s0kaaa@users.noreply.github.com>
2023-11-28 07:16:06 +0000

    creating game info table

--- src/bulid.py
+++ src/bulid.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import date
+import pandas as pd
 
 
 url = "https://api-nba-v1.p.rapidapi.com/games"
@@ -14,4 +15,38 @@ headers = {
 
 response = requests.get(url, headers=headers, params=querystring)
 
-print(response.json())
+data = response.json()
+
+result_count = data["results"]
+
+details = data["response"]
+
+detail_date = []
+detail_arena= []
+detail_teams = []
+detail_scores = []
+detail_officials = []
+
+for i in range(result_count):
+	detail_date = details[i]["date"]
+	detail_arena = details[i]["arena"]
+	detail_teams = details[i]["teams"]
+	detail_scores = details[i]["scores"]
+	detail_officials = details[i]["officials"]
+	teams = pd.DataFrame(detail_teams)
+	team_final = teams.drop("id").drop("nickname").drop("logo")
+	# print(team_final)
+	scores = pd.DataFrame(detail_scores)
+	scores_final = scores.drop("win").drop("loss").drop("series")
+	game =  pd.concat([team_final, scores_final], axis=0) 
+	print(game)
+
+
+# detail_teams_visitors_name = details[0]["teams"]["visitors"]["name"]
+# detail_teams_visitors_code = details[0]["teams"]["visitors"]["code"]
+
+# print(detail_teams)
+
+#table=  pd.DataFrame(detail)
+
+#print(table)
